37bb36c6af5ef6bc3cdf8abfb6bea74e4794aa03
Giselle jacinto <gisellejacinto1219@gmail.com>
2023-10-12 22:02:31 -0300

    07c - SP2 - Atividade 3 - Implementar Service de consumo de API
    07c - SP2 - Atividade 3 - Implementar Service de consumo de API

---
+++ lh-games-loja/src/app/restrito/lista-produto/lista-produto.component.ts
@@ -0,0 +1,54 @@
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/login.service';
+import { Produto } from 'src/app/models/Produto.model';
+import { ProdutoService } from 'src/app/produto.service';
+
+@Component({
+  selector: 'app-lista-produto',
+  templateUrl: './lista-produto.component.html',
+  styleUrls: ['./lista-produto.component.css']
+})
+export class ListaProdutoComponent {
+  public produtos: Produto[] = [];
+  public produto: Produto = new Produto (0,"","","",0);
+
+  constructor(private _produtoService:ProdutoService, private _router: Router,private_loginService:LoginService){}
+
+  ngOnInit(): void {
+    this.listarProdutos();
+    this._loginservice.setMostraMenu(false);
+
+  }
+
+  listarProdutos():void{
+    this._produtoService.getProdutos().subscribe(
+      retornaProduto =>{
+        this.produtos = retornaProduto.map(
+          item => {
+            return new Produto(
+              item.id,
+              item.produto,
+              item.descricao,
+              item.foto,
+              item.preco
+            );
+          }
+        )
+      }
+    )
+  }
+
+  excluir(id: number){
+    this._produtoService.removerProduto(id).subscribe(
+      vaga => {
+        this.listarProdutos();
+      },
+      err => {console.log("Erro ao Excluir")}
+    );
+
+      this._router.navigate(["/restrito/lista"]);
+
+  }
+
+}
